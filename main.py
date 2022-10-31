73ab8eb315f19f0d0d9789aa00a8499db8267575
SparkliTwizzl (she/her) <github@sparklitwizzl.com>
2022-10-31 01:09:18 -0600

    move code into class

--- main.py
+++ main.py
@@ -2,25 +2,27 @@ import interactions
 import os
 
 
-bot = interactions.Client(token=os.getenv("BOT_DISCORD_HEIMDALL_TOKEN")) # any machine without token saved as environment variable cant run the bot
+class bot_heimdall:
+
+    bot = interactions.Client(token=os.getenv("BOT_DISCORD_HEIMDALL_TOKEN")) # any machine without token saved as environment variable cant run the bot
 
 
-# basic message-based command
-# @bot.command(
-#     name="heimdall-message-command",
-#     description="description",
-# )
-# async def heimdall_message_command(ctx: interactions.CommandContext):
-#     await ctx.send("message command invoked")
+    # basic message-based command
+    # @bot.command(
+    #     name="heimdall-message-command",
+    #     description="description",
+    # )
+    # async def heimdall_message_command(ctx: interactions.CommandContext):
+    #     await ctx.send("message command invoked")
 
 
-# context menu command (right click menu)
-# @bot.command(
-#     type=interactions.ApplicationCommandType.USER,
-#     name="heimdall-context-menu-command"
-# )
-# async def heimdall_context_menu_command(ctx):
-#     await ctx.send(f"context menu command invoked on user {ctx.target.user.username}")
+    # context menu command (right click menu)
+    # @bot.command(
+    #     type=interactions.ApplicationCommandType.USER,
+    #     name="heimdall-context-menu-command"
+    # )
+    # async def heimdall_context_menu_command(ctx):
+    #     await ctx.send(f"context menu command invoked on user {ctx.target.user.username}")
 
 
-bot.start()
+    bot.start()
