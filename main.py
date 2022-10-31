725cda537d2ced1e11045a9d82d37b364109e272
SparkliTwizzl (she/her) <github@sparklitwizzl.com>
2022-10-31 00:54:26 -0600

    rename command methods to match command names

--- main.py
+++ main.py
@@ -10,15 +10,16 @@ bot = interactions.Client(token=os.getenv("BOT_DISCORD_HEIMDALL_TOKEN")) # any m
 #     name="heimdall-message-command",
 #     description="description",
 # )
-# async def heimdall_help(ctx: interactions.CommandContext):
+# async def heimdall_message_command(ctx: interactions.CommandContext):
 #     await ctx.send("response")
+
 
 # context menu command (right click menu)
 # @bot.command(
 #     type=interactions.ApplicationCommandType.USER,
 #     name="heimdall-context-menu-command"
 # )
-# async def test(ctx):
+# async def heimdall_context_menu_command(ctx):
 #     await ctx.send(f"context menu command invoked on user {ctx.target.user.username}")
 
 
